95ea5ecbd9fb032d1e03eb8dded4a169729c7716
jhpark <parkpak@gmail.com>
2024-02-04 23:41:19 +0900

    image 경로 수정

--- js/background.js
+++ js/background.js
@@ -2,4 +2,4 @@ const images = ['bg-001.jpg', 'bg-002.jpg', 'bg-003.jpg', 'bg-004.jpg', 'bg-005.
 
 const todayImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url('/img/${todayImage}')`;
+document.body.style.backgroundImage = `url('./img/${todayImage}')`;
